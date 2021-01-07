791d799600177b2954f868a8dd5dcc136b7b1ae0
kingalfarizi <alfarizidito@gmail.com>
2021-01-07 09:05:42 +0700

    materi ke2

---
+++ materi2.py
@@ -0,0 +1,19 @@
+# Menampilkan nama
+'''Hari ini saya belajar python di
+  amcc bersama divisi desktop programming'''
+
+nama = "Dito"
+umur = 18
+print(type(nama))
+print(type(umur))
+
+pertama = 'Amikom Computer Club\n Divisi Desktop Programming'
+print(pertama)
+
+angka1 = 21
+angka2 = 3.5
+
+bagi = angka1//angka2
+print(type(bagi), bagi)
+
+print("Dekstop AMCC-\n"*20)
